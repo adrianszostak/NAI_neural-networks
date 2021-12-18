178a51cae39bf176e1991ca26540827e3560ea63
Hubert Ko <hk190696@gmail.com>
2021-12-18 12:51:36 +0100

    poprawka mlp2

--- pima_indians_diabetes_neutral_networks.py
+++ pima_indians_diabetes_neutral_networks.py
@@ -83,20 +83,13 @@ X_test, y_test = new_X[num_training:], y[num_training:]
 mlp = MLPClassifier(
     hidden_layer_sizes=(120, 84),
     max_iter=30,
-    alpha=1e-4,
-    solver="sgd",
-    verbose=10,
-    random_state=1,
     learning_rate_init=0.1,
 )
 
 mlp2 = MLPClassifier(
-    hidden_layer_sizes=(3, 6),
+    hidden_layer_sizes=(120, 84, 50, 30),
     max_iter=30,
-    alpha=1e-4,
-    solver="sgd",
-    verbose=10,
-    random_state=1,
+    learning_rate_init=0.1,
 )
 
 with warnings.catch_warnings():
@@ -107,7 +100,7 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
     mlp2.fit(X_train, y_train)
 
-print("Training bigger hidden_layer_sizes set score: %f" % mlp.score(X_train, y_train))
-print("Test bigger hidden_layer_sizes set score: %f" % mlp.score(X_test, y_test))
-print("Training smaller hidden_layer_sizes set score: %f" % mlp2.score(X_train, y_train))
-print("Test smaller hidden_layer_sizes set score: %f" % mlp2.score(X_test, y_test))
+print("Training smaller hidden_layer_sizes set score: %f" % mlp.score(X_train, y_train))
+print("Test smaller hidden_layer_sizes set score: %f" % mlp.score(X_test, y_test))
+print("Training bigger hidden_layer_sizes set score: %f" % mlp2.score(X_train, y_train))
+print("Test bigger hidden_layer_sizes set score: %f" % mlp2.score(X_test, y_test))
